c49649b52904532a14330b605eb539593eb1219f
modiribrodie <163874746+modiribrodie@users.noreply.github.com>
2024-08-02 21:27:12 +0200

    Stock checking & workout itensity
    determines the stock status based on the number of cans left.
    
    checks the current heart rate to determine if a workout boost is needed. If the heart rate is 100 bpm or higher, the text 'Boost needed!' is displayed, If not 'Energy levels are high!' is shown.

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
 
 
 // #### Challenge 4: Temperature Suitability
